c38e586b7edc6f68b6eeff9e9aa3638559cd478e
Omotayo Olabode <omotayoolabodeo3@gmail.com>
2022-08-10 09:17:45 +0100

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inec = { "Tayo", "Niyii", "Toke", "Tayo" };
+            var newInec = new HashSet<string>();
+            var similar = inec.Where(i => !newInec.Add(i)).Distinct();
+            Console.WriteLine(String.Join(",", similar));
+            Console.ReadLine();
+        }
+    }
+}
